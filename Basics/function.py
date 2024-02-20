eedcad4da23ec185548a48c81bed02ee77869b74
RiccardoDAndrea <riccardo.dandrea@live.de>
2024-02-20 20:27:44 +0100

    adding the first time a conda venv

--- Basics/function.py
+++ Basics/function.py
@@ -8,5 +8,23 @@ def add_numbers(num1, num2):
     print("Sum", sum)
     return 
 
-add_numbers(3,4)
+def get_full_name(first_name, last_name):
+    full_name = first_name + ' ' + last_name
+    print('The full name is: ', full_name)
+    return 
+
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+x_achse = [1,2,3,4,5,6]
+y_achse = [6,5,4,3,2,1]
+
+def create_lineplot(x = int ,y = int ,title_for_plot = str):
+    plt.plot(x,y)
+    plt.title(title_for_plot)
+    plt.show()
+
+create_lineplot(x_achse, y_achse, 'This is a test')
+
 
