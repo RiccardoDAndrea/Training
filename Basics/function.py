90264c6eb915982abb53840539ae16da29151ea7
RiccardoDAndrea <riccardo.dandrea@live.de>
2024-02-20 16:39:26 +0100

    greet and add_numbers function

--- Basics/function.py
+++ Basics/function.py
@@ -1,1 +1,12 @@
 # T R A I N _ F U N C T I O N 
+def greet(name):
+    print('Hello', name)
+    return
+
+def add_numbers(num1, num2):
+    sum = num1 + num2
+    print("Sum", sum)
+    return 
+
+add_numbers(3,4)
+
